87e017ec6834444b36faae94b05753953f0d0eb8
Okwuolisa Umeokolo <27329255+redenmara@users.noreply.github.com>
2021-04-09 13:02:03 -0400

    no message

--- CampusLoop/ViewControllers/MarketCollectionViewController.swift
+++ CampusLoop/ViewControllers/MarketCollectionViewController.swift
@@ -6,22 +6,15 @@
 //
 
 import UIKit
-<<<<<<< HEAD
 import Parse
-=======
->>>>>>> 1c556b90b91c8cca4b1d1b2e07885ad2f8363ebc
 
 private let reuseIdentifier = "Cell"
 
 class MarketCollectionViewController: UICollectionViewController {
-<<<<<<< HEAD
     
     
     var product = [PFObject]()
     
-=======
-
->>>>>>> 1c556b90b91c8cca4b1d1b2e07885ad2f8363ebc
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,7 +25,6 @@ class MarketCollectionViewController: UICollectionViewController {
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
-<<<<<<< HEAD
         let query = PFQuery(className:"Product")
         query.includeKeys(["product_name", "price", "description", "seller"])
         query.limit = 20
@@ -43,8 +35,6 @@ class MarketCollectionViewController: UICollectionViewController {
                 self.collectionView.reloadData()
             }
         }
-=======
->>>>>>> 1c556b90b91c8cca4b1d1b2e07885ad2f8363ebc
     }
 
     /*
